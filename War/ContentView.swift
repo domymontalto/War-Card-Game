0d8a283ce139bab3b9c3319a14a16008e1bb34a0
domymontalto <mon21002@byui.edu>
2022-03-25 21:29:55 -0600

    Initial Commit

---
+++ War/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  War
+//
+//  Created by Domenico Montalto on 25/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War/WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarApp.swift
+//  War
+//
+//  Created by Domenico Montalto on 25/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
